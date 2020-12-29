710913b74ebdf1654c7695f99ab16739ca2f5bfc
Shilpa2026 <69312544+Shilpa2026@users.noreply.github.com>
2020-12-29 21:25:51 +0530

    Update marks.c

--- marks.c
+++ marks.c
@@ -1,26 +1,17 @@
 #include<stdio.h>
-
-int main()
-{
-	int mark;
-	
-	printf("Enter  the mark of the student :");
-	scanf("%d",&mark);
-	
-	if(mark >= 85 && mark <= 100 )
-		printf("Grade A");
-		
-	else if(mark >= 70)
-		printf("Grade B");
-		
-	else if(mark >= 55)
-		printf("Grade C");
-		
-	else if(mark >= 40 )
-		printf("Grade D");
-		
-	else
-		printf("Grade F");
-		
+int main(){
+        int mark;	
+	printf("Enter the mark of the student :");
+	scanf("%d",&mark);		
+	if(mark >= 85 && mark <= 100 )		
+		printf("Grade A");			
+	else if(mark >= 70 && mark <= 84)		
+		printf("Grade B");			
+	else if(mark >= 55 && mark <= 69)		
+		printf("Grade C");			
+	else if(mark >= 40 && mark <= 54)		
+		printf("Grade D");			
+	else if(mark < 40)	
+		printf("Grade F");			
 	return 0;
 }
